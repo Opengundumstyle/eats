2d4463892209e216bc2975f9b95de734d7905c75
Zhile Lin <zhlin04@gmail.com>
2022-07-11 17:07:37 -0700

    heroku update

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -42019,6 +42019,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _store_store__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./store/store */ "./frontend/store/store.js");
 /* harmony import */ var _components_root__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/root */ "./frontend/components/root.jsx");
 /* harmony import */ var _actions_session_actions__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./actions/session_actions */ "./frontend/actions/session_actions.js");
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _readOnlyError(name) { throw new TypeError("\"" + name + "\" is read-only"); }
+
 
 
 
@@ -42031,7 +42035,22 @@ document.addEventListener("DOMContentLoaded", function () {
   window.logout = _actions_session_actions__WEBPACK_IMPORTED_MODULE_4__.logout;
   var store = (0,_store_store__WEBPACK_IMPORTED_MODULE_2__["default"])();
   console.log(store.getState());
-  window.store = store;
+
+  if (window.currentUser) {
+    var preloadedState = {
+      entities: {
+        users: _defineProperty({}, window.currentUser.id, window.currentUser)
+      },
+      session: {
+        id: window.currentUser.id
+      }
+    };
+    (0,_store_store__WEBPACK_IMPORTED_MODULE_2__["default"])(preloadedState), _readOnlyError("store");
+    delete window.currentUser;
+  } else {
+    (0,_store_store__WEBPACK_IMPORTED_MODULE_2__["default"])(), _readOnlyError("store");
+  }
+
   var root = document.getElementById("root"); // ReactDOM.render(<Root store={store}/>, root);
 
   react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", null, "Heroku"), root);
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -12,7 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const store = configureStore()
   console.log(store.getState())
-  window.store = store
+  
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   
 
   const root = document.getElementById("root");
