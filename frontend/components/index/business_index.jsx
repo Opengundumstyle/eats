f5846d232c5fc4b01db01e938bfb19712ca4570f
Zhile Lin <zhlin04@gmail.com>
2023-03-10 15:16:56 -0800

    changed class component to function component & added pagination

--- frontend/components/index/business_index.jsx
+++ frontend/components/index/business_index.jsx
@@ -1,47 +1,113 @@
-import React from 'react';
+import React,{ useState,useEffect} from 'react';
 import IndexItem from './business_index_item'
 import  Map from '../map/map';
 import  Navbar from '../nav/navbar_container'
 import Filter from '../filter/filter';
 
 
-class BusinessIndex extends React.Component {
+// class BusinessIndex extends React.Component {
 
-    componentDidMount() {
-        this.props.getAll()
+//     componentDidMount() {
+//         this.props.getAll()
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                  <div className='list-nav'>< Navbar/></div>
+//             <div className='businesses-index-page-box'>
+//                  <div className='filter-div'>
+//                     <Filter props={this.props}/>
+//                  </div>
+//                    <div className='buinesses-index-page'>
+//                       {this.props.businesses.map((business,index) =>(
+//                                  <IndexItem 
+//                                     business={business}
+//                                     index={index}
+//                                     key={business.id}
+//                                     price={business.price}
+//                                     openhour={business.openhour}
+//                                     closehour={business.closehour}
+//                                     rating = {business.rating}
+//                                     review = {business.review}
+//                                     website= {business.website}
+//                                     categories ={business.categoriesItem}/>
+//                                            ))}
+//                     </div>
+//                   <div className='businesses-map'>
+//                       <Map businesses={this.props.businesses}/>
+//                   </div>
+//                </div>
+//             </div>
+//         )
+//     }
+//   }
+
+// export default BusinessIndex
+
+function BusinessIndex(props) {
+
+    const [currentPage,setCurrentPage] = useState(1)
+    const [itemsPerPage,setItemPerPage] = useState(10)
+    
+    useEffect(() => {
+      props.getAll();
+    }, []);
+
+    const indexOfLastItem = currentPage * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentItems = props.businesses.slice(indexOfFirstItem,indexOfLastItem)
+
+    const totalPages = Math.ceil(props.businessness.length/itemsPerPage)
+    const pageNumbers  = []
+
+    for(let  i = 0; i < totalPages;i++){
+         pageNumbers.push(i)
     }
 
-    render() {
-        return (
-            <div>
-                 <div className='list-nav'>< Navbar/></div>
-            <div className='businesses-index-page-box'>
-                 <div className='filter-div'>
-                    <Filter props={this.props}/>
-                 </div>
-                   <div className='buinesses-index-page'>
-                      {this.props.businesses.map((business,index) =>(
-                                 <IndexItem 
-                                    business={business}
-                                    index={index}
-                                    key={business.id}
-                                    price={business.price}
-                                    openhour={business.openhour}
-                                    closehour={business.closehour}
-                                    rating = {business.rating}
-                                    review = {business.review}
-                                    website= {business.website}
-                                    categories ={business.categoriesItem}/>
-                                           ))}
-                    </div>
-                  <div className='businesses-map'>
-                      <Map businesses={this.props.businesses}/>
-                  </div>
-               </div>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <div className='list-nav'>
+          <Navbar />
+        </div>
+        <div className='businesses-index-page-box'>
+          <div className='filter-div'>
+            <Filter props={props} />
+          </div>
+          <div>Test</div>
+          <div className='buinesses-index-page'>
+            {currentItems.map((business, index) => (
+              <IndexItem
+                business={business}
+                index={index}
+                key={business.id}
+                price={business.price}
+                openhour={business.openhour}
+                closehour={business.closehour}
+                rating={business.rating}
+                review={business.review}
+                website={business.website}
+                categories={business.categoriesItem}
+              />
+            ))}
+          </div>
+          <div className='businesses-map'>
+            <Map businesses={props.businesses} />
+          </div>
+          <div className='pagination'>
+              {
+                pageNumbers.map(
+                    number =>(
+                        <button key ={number} onClick={()=>setCurrentPage(number)}>
+                                {number}
+                        </button>
+                    )
+                )
+              }
+          </div>
+        </div>
+      </div>
+    );
   }
-
-export default BusinessIndex
-
+  
+  export default BusinessIndex;
---
+++ frontend/components/paginatedlst/paginatedList.jsx
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+
+export default function PaginatedList({itemsPerPage,items}){
+       
+     
+
+  return(
+     <div></div>
+  )
+
+}
+
